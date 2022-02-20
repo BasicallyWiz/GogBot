305be8c79525ba9b2b327296eb93a08061fb464f
BasicallyWiz <shushdasher@gmail.com>
2022-02-19 19:20:08 -0800

    I have restarted. Again. gogSerious I have standards that I for some reason deviated. The only way back now is to restart the project.

---
+++ GogBot/Core/GogBotInstance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GogBot.Core
+{
+  public class GogBotInstance
+  {
+
+    public GogBotInstance()
+    {
+
+    }
+  }
+}
